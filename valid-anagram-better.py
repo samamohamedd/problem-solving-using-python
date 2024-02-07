925ada5453f57155c9dadba75f5273ef1ff511a1
Sama mohamed mahmoud abd ellatif <sama.mohamed@std.mans.edu.eg>
2024-02-07 03:22:54 +0200

    Update valid-anagram-better.py

---
+++ valid-anagram-better.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        #return sorted(s) == sorted(t)
+        if len(s) !=len(t):
+            return False
+        countS , countT = {}, {}
+
+        for i in range(len(s)):
+            countS[s[i]] = countS.get(s[i], 0) + 1
+            countT[t[i]] = countT.get(t[i], 0) + 1
+        for c in countS:
+            if countS[c] != countT.get(c, 0):
+                return False
+        return True
+    
