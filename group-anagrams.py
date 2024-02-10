6bf3fb2a6ea40b2afe946c3d1aff6df4e3708cb6
Sama mohamed mahmoud abd ellatif <sama.mohamed@std.mans.edu.eg>
2024-02-10 18:27:24 +0200

    s

---
+++ group-anagrams.py
@@ -0,0 +1,16 @@
+class Solution:
+    def groupAnagrams(self, strs):
+        finalList = []
+        if len(strs) <= 1: return [strs]
+        finalList.append([strs[0]])
+        for i in range(1,len(strs)): 
+            for innerlist in finalList:
+                if sorted(strs[i]) == sorted(innerlist[0]):
+                    innerlist.append(strs[i])
+                    break    
+            else:
+                finalList.append([strs[i]])
+        return finalList
+            
+s = Solution()
+print(s.groupAnagrams(["eat","tea","tan","ate","nat","bat"]))
